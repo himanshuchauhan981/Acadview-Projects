14dfd25503dbadea8bd0d7aa74b6d07879127292
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-03-12 15:25:52 +0530

    Create showFormData.js

---
+++ Assignment 5 - React Weather API Project/src/components/showFormData.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class ShowFormData extends Component{
+   render(){
+      return(
+         <div>
+            <p>This is form data</p>
+         </div>
+      )
+   }
+}
+
+export default ShowFormData;
