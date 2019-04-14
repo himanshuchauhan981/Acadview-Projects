8a3d4a1c1713fabbdef40700579ed43f007441db
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2019-04-14 22:56:41 +0530

    Delete index.js

--- Assignment 7 - MongoDB/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/testDatabase',{useNewUrlParser: true})
-
-mongoose.connection.once('open',()=>{
-    console.log('Mongoose Connection is successfull')
-}).on('error',(error)=>{
-    console.log('Connection error : '+error)
-})
